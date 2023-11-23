7b61139c0f2f9a0d0895e649153e6ec83839053a
Valera200707 <valera-alekseev-2007@inbox.ru>
2023-11-23 19:50:50 +0300

    first task

---
+++ main.py
@@ -0,0 +1,47 @@
+import sqlite3
+import sys
+
+from PyQt5 import uic, QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.read()
+
+    def read(self):
+        con = sqlite3.connect('coffee.sqlite')
+        cur = con.cursor()
+        result = cur.execute("SELECT * FROM coffees").fetchall()
+
+        con.close()
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setHorizontalHeaderLabels(
+            ['ID', 'название сорта', 'степень обжарки', 'молотый/в зернах', 'описание вкуса', 'цена', 'объем упаковки'])
+        for n, i in enumerate(result):
+            for j in range(7):
+                self.tableWidget.setItem(n, j, QtWidgets.QTableWidgetItem(str(i[j])))
+
+
+def exception_hook(exctype, value, traceback):
+    print(exctype, value, traceback)
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+    if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
+    sys._excepthook = sys.excepthook
+    sys.excepthook = exception_hook
+
+    app = QApplication(sys.argv)
+    w = MyWindow()
+    w.show()
+    sys.exit(app.exec())
